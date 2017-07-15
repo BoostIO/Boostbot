7a5dd6689dbbafe57c47ff11b64d148587265bc4
asmsuechan <suenagaryoutaabc@gmail.com>
2017-07-15 18:39:15 +0900

    Fix for cron task

--- dailyCronTask.js
+++ dailyCronTask.js
@@ -3,24 +3,27 @@ const slackWebhook = require('./slackWebhook')
 const GitHub = require('github-api')
 const Repository = GitHub.Repository
 const fs = require('fs')
+const CronJob = require('cron').CronJob
 require('dotenv').config()
 
 const GitHubAccessToken = process.env.GITHUB_ACCESS_TOKEN
 const url = process.env.SLACK_WEBHOOK_URL
 const slack = new slackWebhook(url)
 
-const organization = 'BoostIO'
-const repository = 'Boostnote'
-const gh = new GitHub({ token: GitHubAccessToken })
-const repo = gh.getRepo(organization, repository)
+new CronJob('59 59 23 * * *', () => {
+  const organization = 'BoostIO'
+  const repository = 'Boostnote'
+  const gh = new GitHub({ token: GitHubAccessToken })
+  const repo = gh.getRepo(organization, repository)
 
-const filename = 'star-count.txt'
-const yesterday = fs.readFileSync(filename)
+  const filename = 'star-count.txt'
+  const yesterday = fs.readFileSync(filename)
 
-repo.getDetails().then(res => {
-  const starCount = res.data.stargazers_count
-  const difference = parseInt(starCount) - parseInt(yesterday)
-  const message = `@here It's time to notice the star counts!\nTotal: ${starCount}\nToday: ${difference}`
-  slack.sendMessage(message)
-  fs.writeFile(filename, starCount)
-})
+  repo.getDetails().then(res => {
+    const starCount = res.data.stargazers_count
+    const difference = parseInt(starCount) - parseInt(yesterday)
+    const message = `@here It's time to notice the star counts!\nTotal: ${starCount}\nToday: ${difference}`
+    slack.sendMessage(message)
+    fs.writeFile(filename, starCount)
+  })
+}, null, true, 'Japan')
